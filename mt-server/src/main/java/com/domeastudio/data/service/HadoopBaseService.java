a59bbe2993a6e6d33da43af31a444f52460aa476
DomeA <domeastudio@gmail.com>
2016-04-10 18:18:30 +0800

    Rejected commit: Default

---
+++ mt-server/src/main/java/com/domeastudio/data/service/HadoopBaseService.java
@@ -0,0 +1,13 @@
+package com.domeastudio.data.service;
+
+
+import org.apache.log4j.Logger;
+
+import java.io.Serializable;
+
+/**
+ * Created by domea on 16-1-17.
+ */
+public class HadoopBaseService<T extends Serializable> {
+    private static Logger logger = Logger.getLogger(HadoopBaseService.class);
+}
---
+++ mt-server/src/main/java/com/domeastudio/data/service/RServices/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by domea on 16-4-10.
+ */
+package com.domeastudio.data.service.RServices;
