f6e8826b6950851b3a03b7429b6d459d1dd64ea5
DomeA <domeastudio@gmail.com>
2016-05-25 21:42:59 +0800

    Rejected commit: Default

--- mt-server/src/main/java/com/domeastudio/rest/block/table/DataTable.java
+++ mt-server/src/main/java/com/domeastudio/rest/block/table/DataTable.java
@@ -1,9 +1,12 @@
 package com.domeastudio.rest.block.table;
 
+import com.domeastudio.dto.UserOperationParams;
 import com.sun.jersey.spi.resource.Singleton;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by domea on 16-5-25.
@@ -12,4 +15,30 @@ import javax.ws.rs.Path;
 @Component
 @Singleton
 public class DataTable {
+    private static Logger logger = Logger.getLogger(DataTable.class);
+
+    @GET
+    @Path("/tables")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getTables(UserOperationParams userOperationParams){
+        return null;
+    }
+
+    @GET
+    @Path("/table/{ttid}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getTable(@PathParam("ttid") String ttid){
+        return null;
+    }
+
+    @POST
+    @Path("/addTable")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String addTable(@FormParam("tableName") String tableName){
+        return null;
+    }
+
 }
