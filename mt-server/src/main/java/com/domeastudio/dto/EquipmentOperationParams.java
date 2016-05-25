6dd47758d5f180c99104da280d3c4dac91b37521
DomeA <domeastudio@gmail.com>
2016-05-25 20:20:21 +0800

    Rejected commit: Default

---
+++ mt-server/src/main/java/com/domeastudio/dto/EquipmentOperationParams.java
@@ -0,0 +1,18 @@
+package com.domeastudio.dto;
+
+import java.util.Date;
+
+/**
+ * Created by domea on 16-5-25.
+ */
+public class EquipmentOperationParams {
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ mt-server/src/main/java/com/domeastudio/dto/UserOperationParams.java
@@ -0,0 +1,57 @@
+package com.domeastudio.dto;
+
+import org.codehaus.jackson.map.annotate.JsonRootName;
+
+import java.util.Date;
+
+/**
+ * Created by domea on 16-5-25.
+ */
+@JsonRootName("userOperationParams")
+public class UserOperationParams {
+    private String email;
+    private String phone;
+    private String loginName;
+    private String passWord;
+    private Date date;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
